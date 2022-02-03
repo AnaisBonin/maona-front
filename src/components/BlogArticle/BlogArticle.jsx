e40d2a7eefa1cfa9d2522f2566f737cafb23153b
AnaisBonin <anaisboninfr@gmail.com>
2022-02-03 12:05:36 +0100

    Updated product card to display pictures

--- src/components/BlogArticle/BlogArticle.jsx
+++ src/components/BlogArticle/BlogArticle.jsx
@@ -1,16 +1,35 @@
-import BlogParagraph from '../BlogParagraph/BlogParagraph';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-import image from '../../assets/images/Atelier.jpg'
+import BlogParagraph from '../BlogParagraph/BlogParagraph';
 
 import './BlogArticle.css';
 
 const BlogArticle = () => {
+  const [article, setArticle] = useState();
+
+  const fetchArticle = async () => {
+    const { data } = await axios.get(
+      `${process.env.REACT_APP_API_URL}/articles`,
+    );
+    return setArticle(data);
+  };
+
+  useEffect(() => {
+    fetchArticle();
+  }, []);
+
   return (
-    <article>
-      <h2>Article Title</h2>
-      <img src={image} alt="Picture of a workshop" />
-      <BlogParagraph />
-    </article>
+    <>
+      {article &&
+        article.map((article, i) => (
+          <article className="article-container" key={i}>
+            <h2>{article.title}</h2>
+            <img src={`${process.env.REACT_APP_API_URL}${article.img}`} alt="Picture of a workshop" />
+            <BlogParagraph />
+          </article>
+        ))}
+    </>
   );
 };
 
